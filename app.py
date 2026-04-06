39b87b2b55e7e1d9e8e9d4e32dd2e315114d339e
dtrefilova-cyber <d.trefilova@cleack.com>
2026-04-06 15:35:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ def transcribe_audio(url):
             "https://api.deepgram.com/v1/listen",
             headers={"Authorization": f"Token {DEEPGRAM_API_KEY}"},
             params={
-                "model": "general",
+                "model": "nova-3",
                 "smart_format": "true",
                 "punctuate": "true",
                 "utterances": "true",
